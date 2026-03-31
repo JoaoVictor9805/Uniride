98975e9957c8f59b25df85a342618ca47e80a85c
JoaoVictor9805 <joaoczech9805@gmail.com>
2026-03-31 16:41:02 -0300

    --Form Motorista html e javascript (falta php)

--- javascript/cadastro.js
+++ javascript/cadastro.js
@@ -2,12 +2,47 @@ document.getElementById("enviar").addEventListener('click', function () {
     novo();
 });
 
+document.getElementById("motorista").addEventListener('change', function () {
+    mostrarFormMotorista();
+});
+
+
+function mostrarFormMotorista() {
+    var x = document.getElementById("formMotorista");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
 async function novo() {
     var nome = document.getElementById("nome").value;
     var senha = document.getElementById("senha").value;
     var email = document.getElementById("email").value;
     var nascimento = document.getElementById("nascimento").value;
     var motorista = document.getElementById("motorista").checked;
+
+    
+    if (this.motorista == true) {
+        await cadastrarMotorista();
+        var dataVencimento = document.getElementById("dataVencimento").value;
+        var numeroRegistro = document.getElementById("numeroRegistro").value;
+        var cpf = document.getElementById("cpf").value;
+
+        const fdMotorista = new FormData();
+        fdMotorista.append('dataVencimento', dataVencimento);
+        fdMotorista.append('numeroRegistro', numeroRegistro);
+        fdMotorista.append('cpf', cpf);
+
+        const retornoMotorista = await fetch("../php/cadastro_motorista.php", {
+            method: "POST",
+            body: fdMotorista
+        });
+
+        const respostaMotorista = await retornoMotorista.json();
+    }
 
     const fd = new FormData();
     fd.append('nome', nome);
@@ -31,3 +66,4 @@ async function novo() {
     }
 
 }
+
