aeb70306c72279fa9204a62eb70c3b85e6f26ebb
brunompz <brunomartinsparizotto@gmail.com>
2026-03-31 09:31:28 -0300

    criação CRUDs básicos sem php.

---
+++ index.js
@@ -0,0 +1,34 @@
+document.getElementById("entrar").addEventListener('click',function(){
+    login();
+    verificacao();
+});
+
+document.getElementById("cadastra").addEventListener('click',function(){
+    window.location.href = 'cadastro/cadastro.html'
+});
+
+function verificacao(){
+    
+}
+
+async function login(){
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+    if (!email || !senha === null){
+        alert("Preencha seus dados de login!");
+        localStorage.clear("sessao");
+        window.location.reload;
+    } else{
+    const fd = new FormData();
+    fd.append("email", email);
+    fd.append("senha", senha);
+
+    const retorno = await fetch("php/login.php",{
+        method: "POST",
+        body: fd
+    });
+    const resposta = await retorno.json();
+    localStorage.setItem("sessao",JSON.stringify(resposta));
+    window.location.href = "home/index.html";
+    };
+};
